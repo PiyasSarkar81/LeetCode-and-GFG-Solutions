f22fa031c317998e2d0f4cb7d7850ea56a580061
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-03-13 12:37:49 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -2,6 +2,12 @@ class Solution {
 public:
     int fib(int n) {
         if(n<2) return n;
-        return fib(n-1)+fib(n-2);
+        int prev = 1, prev2 = 0, ans;
+        for(int i = 1;i<n;i++){
+            ans = prev + prev2;
+            prev2 = prev;
+            prev = ans;
+        }
+        return ans;
     }
 };
