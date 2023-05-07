d5a3d9ddab6ac276d48b5ced36fa720ef8b7f152
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-05-07 08:34:34 +0530

    Added solution - LeetHub

---
+++ Rotating an Array - GFG/rotating-an-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void leftRotate(int arr[], int n, int d) {
+        // code here
+        d = d % n;
+        rev(arr,0,d-1);
+        rev(arr,d,n-1);
+        rev(arr,0,n-1);
+    }
+    
+    void rev(int arr[], int l, int r){
+        while(l<r){
+            swap(arr[l],arr[r]);
+            l++,r--;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends
