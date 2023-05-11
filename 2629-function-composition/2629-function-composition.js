c0fee2da5e4485d40eca9e24bf2a9a5a8296811c
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-05-11 10:01:29 +0530

    Time: 61 ms (95.56%), Space: 43.1 MB (74.38%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    const fn = (acc,f) => f(acc);
+	return function(x) {
+        return functions.reduceRight(fn,x);
+
+        // for(const fn of functions.reverse()){
+        //     x = fn(x);
+        // }
+        // return x;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
