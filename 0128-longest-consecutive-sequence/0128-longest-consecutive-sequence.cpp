87f1875372301f0feed7e743a143c6957952a55e
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-05-14 12:28:53 +0530

    Time: 82 ms (81.33%), Space: 33.6 MB (97.16%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        set<int>hashSet;
-        for(int num : nums){
-            hashSet.insert(num);
-        }
-        int longestStreak =0;
-        for(int num:nums){
-            if(!hashSet.count(num-1)){
-                int currentNum =num;
-                int currentStreak =1;
-                while(hashSet.count(currentNum+1)){
-                    currentNum += 1;
-                    currentStreak += 1;
+        // set<int>hashSet;
+        // for(int num : nums){
+        //     hashSet.insert(num);
+        // }
+        // int longestStreak =0;
+        // for(int num:nums){
+        //     if(!hashSet.count(num-1)){
+        //         int currentNum =num;
+        //         int currentStreak =1;
+        //         while(hashSet.count(currentNum+1)){
+        //             currentNum += 1;
+        //             currentStreak += 1;
+        //         }
+        //         longestStreak = max(longestStreak,currentStreak);
+        //     }
+        // }
+        // return longestStreak;
+        
+
+        if(nums.size() == 0) return 0;
+        int cnt = 1, maxLength = 1;
+        sort(nums.begin(), nums.end());
+        for(int i =1;i<nums.size();i++) {
+            if(nums[i] != nums[i-1]) {
+                if(nums[i] == nums[i-1] + 1) {
+                    cnt++;
+                } else {
+                    maxLength = max(maxLength, cnt);
+                    cnt = 1;
                 }
-                longestStreak = max(longestStreak,currentStreak);
             }
         }
-        return longestStreak;
+        return max(maxLength, cnt);
     }
 };
