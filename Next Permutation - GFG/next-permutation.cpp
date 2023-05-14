07943208c51e317b2a1515bb1fde9d2caa774358
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-05-14 10:38:29 +0530

    Added solution - LeetHub

--- Next Permutation - GFG/next-permutation.cpp
+++ Next Permutation - GFG/next-permutation.cpp
@@ -9,10 +9,29 @@ using namespace std;
 
 class Solution{
 public:
-    vector<int> nextPermutation(int N, vector<int> arr){
+    vector<int> nextPermutation(int n, vector<int> arr){
         // code here
-        next_permutation(arr.begin(), arr.end());
+        // next_permutation(arr.begin(), arr.end());
+        // return arr;
+        
+        int i,j;
+        for(i = n-2;i>=0;i--) {
+            if(arr[i] < arr[i+1]) {
+                break;
+            }
+        }
+        if(i<0) reverse(arr.begin(), arr.end());
+        else {
+            for(int j =n-1;j>=i;j--) {
+                if(arr[j] > arr[i] ) {
+                    swap(arr[i], arr[j]);
+                    break;
+                }
+            }
+            reverse(arr.begin() + i+ 1, arr.end());
+        }
         return arr;
+        
     }
 };
 
