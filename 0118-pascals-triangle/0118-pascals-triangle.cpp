e060cb3e721a029e29bad581e7ed1889cb15f7e0
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2023-05-16 10:46:23 +0530

    Time: 5 ms (30.29%), Space: 6.7 MB (79.62%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
+    // vector<vector<int>> generate(int numRows) {
+        // vector<vector<int>> r(numRows);
+        // for(int i=0;i<numRows;i++){
+        //     r[i].resize(i+1);
+        //     r[i][i]=r[i][0]=1;
+        //     for(int j=1;j<i;j++){
+        //         r[i][j]=r[i-1][j]+r[i-1][j-1];
+        //     }
+        // }
+        // return r; 
+    // }
+    
+     vector<int> genarateRow(int row) {
+        int ans = 1;
+        vector<int> temp;
+        temp.push_back(1);
+        for(int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans /= col;
+            temp.push_back(ans);
+        }
+        return temp;
+    }
+
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> r(numRows);
-        for(int i=0;i<numRows;i++){
-            r[i].resize(i+1);
-            r[i][i]=r[i][0]=1;
-            for(int j=1;j<i;j++){
-                r[i][j]=r[i-1][j]+r[i-1][j-1];
-            }
+        vector<vector<int>> r;
+        for(int i=1;i<=numRows; i++) {
+            r.push_back(genarateRow(i));
         }
         return r;
     }
