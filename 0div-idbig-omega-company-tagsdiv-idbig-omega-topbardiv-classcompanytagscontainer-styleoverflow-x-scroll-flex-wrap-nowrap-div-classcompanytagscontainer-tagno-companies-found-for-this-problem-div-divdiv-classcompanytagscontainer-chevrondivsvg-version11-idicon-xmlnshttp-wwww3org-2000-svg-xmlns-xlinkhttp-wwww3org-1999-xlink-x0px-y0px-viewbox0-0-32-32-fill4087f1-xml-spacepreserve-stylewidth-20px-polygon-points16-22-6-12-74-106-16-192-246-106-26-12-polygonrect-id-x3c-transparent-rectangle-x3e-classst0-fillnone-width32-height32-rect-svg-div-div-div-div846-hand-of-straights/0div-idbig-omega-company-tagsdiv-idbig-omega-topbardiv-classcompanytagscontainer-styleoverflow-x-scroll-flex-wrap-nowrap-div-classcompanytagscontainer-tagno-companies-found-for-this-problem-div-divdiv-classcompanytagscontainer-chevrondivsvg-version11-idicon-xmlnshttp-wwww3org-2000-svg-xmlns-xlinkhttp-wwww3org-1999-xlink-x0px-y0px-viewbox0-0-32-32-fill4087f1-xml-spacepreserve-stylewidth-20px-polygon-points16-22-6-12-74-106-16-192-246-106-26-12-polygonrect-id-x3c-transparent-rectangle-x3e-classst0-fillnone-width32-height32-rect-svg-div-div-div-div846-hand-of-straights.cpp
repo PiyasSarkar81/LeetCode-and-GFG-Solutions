35306e6df965a44f40c6945ff346465b53b0c65a
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2024-06-06 08:34:53 +0530

    Time: 58 ms (62.52%), Space: 30.2 MB (86.99%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div846-hand-of-straights/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div846-hand-of-straights.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        unordered_map<int,int> countMap;
+        for(int card : hand) countMap[card]++;
+        sort(hand.begin(), hand.end());
+        for(int i=0;i<hand.size();i++){
+            if(countMap[hand[i]] == 0) continue;
+            for(int j=0;j<groupSize;j++){
+                int currCard = hand[i] + j;
+                if(countMap[currCard] == 0) return false;
+                countMap[currCard]--;
+            }
+        }
+        return true;
+    }
+};
