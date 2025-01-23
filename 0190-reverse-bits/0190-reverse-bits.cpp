dfc78035c405dbf4e0bbb0eb054d96507efc0737
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-01-23 20:07:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (27.6%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,37 +1,12 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        bool flag = false;
-        int rev =0;
-        int j =0;
-        
-        for(int i =31; i>=0;i--){
-            int mask = (1<<i);
-            
-            if(flag){
-                if((n&mask) !=0){
-                    cout<<1;
-                    
-                    int smask = (1<<j);
-                    rev |= smask;
-                    
-                }else{
-                    cout<<0;
-                }
-                j++;
-            }
-            else{
-                if((n&mask)!=0){
-                    flag = true;
-                    cout<<1;
-                    
-                    int smask = (1 <<j);
-                    rev |= smask;
-                    
-                }
-                j++;
-            }
+        uint32_t ans = 0;
+        for(int i=0;i<32;i++){
+            ans <<= 1;
+            ans |= (n&1);
+            n >>= 1; 
         }
-        return rev;
+        return ans;
     }
 };
