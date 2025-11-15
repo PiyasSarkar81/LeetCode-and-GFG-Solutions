808d3e0c40e6de5acc91e1d20ca31c348ec0fdfe
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-11-15 21:11:33 +0530

    Time: 40 ms (90.18%), Space: 29.1 MB (72.58%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        vector<vector<int>> res;
-        
-        for(int i=0; i<nums.size()-2;i++){
-            if(i==0 || (i>0 && nums[i] != nums[i-1])){
-                int lo = i+1, hi = nums.size()-1, sum = 0 - nums[i];
-                while(lo<hi){
-                    if(nums[lo]+nums[hi]==sum){
-                        vector<int> temp;
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[lo]);
-                        temp.push_back(nums[hi]);
-                        res.push_back(temp);
-                        
-                        while(lo<hi && nums[lo]==nums[lo+1]) lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1]) hi--;
-                        
-                        lo++; hi--;
-                    }
-                    else if(nums[lo]+nums[hi]<sum) lo++;
-                    else hi--;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i=0;i<nums.size()-2;i++){
+            if(nums[i] > 0) break;
+            if( i>0 && nums[i] ==  nums[i-1]) continue;
+            int l = i+1, r = nums.size()-1;
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == 0) {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    // ⭐ Skip duplicates for left pointer
+                    while(l < r && nums[l] == nums[l + 1]) l++;
+
+                    // ⭐ Skip duplicates for right pointer
+                    while(l < r && nums[r] == nums[r - 1]) r--;
+                    r--,l++;
                 }
+                else if(sum > 0) r--;
+                else l++;
             }
         }
-        return res;
+        return ans;
     }
 };
