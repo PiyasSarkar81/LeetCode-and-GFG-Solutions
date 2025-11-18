f5d73316a04b267fee306d6b23f7f7ccca2af99b
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-11-18 22:26:05 +0530

    Time: 8 ms (96.54%), Space: 79.5 MB (93.12%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,40 +2,21 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
         int n = heights.size();
-        vector<int> left(n),right(n);
-        
-        stack<int> myStack;
-        for(int i =0;i<n;i++){
-            if(myStack.empty()){
-                left[i] = 0;
-                myStack.push(i);
+        stack<int> st;
+        int maxArea = 0;
+
+        for(int i=0;i<=n;i++){
+            while(!st.empty() && (i == n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i - st.top() -1;
+                maxArea = max(maxArea, height* width);
             }
-            else{
-                while(!myStack.empty() && heights[myStack.top()]>=heights[i])
-                    myStack.pop();
-                left[i] = myStack.empty()? 0 : myStack.top()+1;
-                myStack.push(i);
-            }
+
+            st.push(i);
         }
-        while(!myStack.empty())
-            myStack.pop();
-        
-        for(int i =n-1;i>=0;i--){
-            if(myStack.empty()){
-                right[i] = n-1;
-                myStack.push(i);
-            }
-            else{
-                while(!myStack.empty() && heights[myStack.top()] >= heights[i])
-                    myStack.pop();
-                right[i] = myStack.empty()? n-1: myStack.top()-1;
-                myStack.push(i);
-            }
-        }
-        int max_area =0;
-        for(int i =0;i<n;i++){
-            max_area = max(max_area, heights[i]*(right[i]-left[i]+1));
-        }
-        return max_area;
+
+        return maxArea;
+
     }
 };
