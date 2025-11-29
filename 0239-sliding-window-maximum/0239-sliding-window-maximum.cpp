efdf3865abe1ec5c879c6d37a990e3c31e51fe7b
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-11-29 20:41:19 +0530

    Time: 2123 ms (5.06%), Space: 159.3 MB (10.82%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<pair<int,int>> dq;
-        vector<int> ans;
-        
-        for(int i =0;i<nums.size();i++){
-            if(!dq.empty() && dq.front().second<=(i-k))
-                dq.pop_front();
-            while(!dq.empty() && dq.back().first<nums[i])
-                dq.pop_back();
-            dq.push_back(make_pair(nums[i],i));
-            
-            if(i>=(k-1))
-                ans.push_back(dq.front().first);
+        int n = nums.size();
+        int l =0;
+        vector<int> ans(n-k+1);
+        unordered_map<int,int> mp;
+        for(int i=0;i<k;i++) mp[nums[i]]++;
+
+        ans[0] = *max_element(nums.begin(), nums.begin()+k);
+        while(l+k<n){
+            mp[nums[l]]--;
+            if(mp[nums[l]] == 0) mp.erase(nums[l]);
+            mp[nums[l+k]]++;
+            if(nums[l] == ans[l]) {
+                int val= INT_MIN;
+                for(auto m : mp) val = max(val,m.first);
+                ans[l+1] = val;
+            }
+            else ans[l+1] = max(ans[l], nums[l+k]);
+            l++;
+
         }
         return ans;
+
     }
 };
