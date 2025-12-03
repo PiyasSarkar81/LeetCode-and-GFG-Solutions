eac2e999ab61cbf630487df11453c13b1d3a8771
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-12-03 19:57:05 +0530

    Time: 3 ms (35.23%), Space: 77.2 MB (13.13%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,25 +11,37 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode * dummy = new ListNode();
-        ListNode * temp = dummy;
-        int carry =0;
-        while(l1!=NULL||l2!=NULL||carry){
-            int sum=0;
-            if(l1!=NULL){
-                sum+=l1->val;
-                l1=l1->next;
-            }
-            if(l2!=NULL){
-                sum+=l2->val;
-                l2=l2->next;
-            }
-            sum+=carry;
-            carry=sum/10;
-            ListNode* node = new ListNode(sum%10);
-            temp->next=node;
-            temp=temp->next;
+        ListNode* head = new ListNode();
+        ListNode* temp = head;
+        int c = 0;
+        while(l1 != nullptr && l2 != nullptr){
+            int val = l1->val + l2->val + c;
+            c = val/10;
+            temp -> next = new ListNode(val%10);
+            temp = temp -> next;
+            l1 = l1 -> next;
+            l2 = l2 -> next;
         }
-        return dummy -> next;
+        while(l1 != nullptr){
+            int val = l1-> val + c;
+            c = val/10;
+            temp -> next = new ListNode(val%10);
+            temp = temp -> next;
+            l1 = l1 -> next;
+        }
+        while(l2 != nullptr){
+            int val = l2-> val + c;
+            c = val/10;
+            temp -> next = new ListNode(val%10);
+            temp = temp -> next;
+            l2 = l2 -> next;
+        }
+        while(c){
+            temp -> next = new ListNode(c%10);
+            temp = temp -> next;
+            c /= 10;
+        }
+
+        return head -> next;
     }
 };
