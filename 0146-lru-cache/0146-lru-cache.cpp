a70f5e4625cde4bd021fd29e19fa293c58b9bafa
Piyas Sarkar <96674830+PiyasSarkar81@users.noreply.github.com>
2025-12-04 21:27:12 +0530

    Time: 103 ms (30.81%), Space: 178 MB (49.34%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,40 +1,67 @@
+class Node {
+    public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
 
+    Node(int k, int v): key(k), val(v), prev(nullptr), next(nullptr) {}
+};
 
 class LRUCache {
-     list<int>l;
-     unordered_map<int,pair<list<int>::iterator, int>>m;
-     int size;
-public:
-    LRUCache(int capacity) {
-         size = capacity;
-    }
-    
-    void moveToFirst(int key){
-        l.erase(m[key].first);
-        l.push_front(key);
-        m[key].first = l.begin();
+    int cap;
+    unordered_map<int, Node*> cache;
+    Node* left;
+    Node* right;
+
+    void remove(Node* node){
+        Node* prev = node -> prev;
+        Node* nxt = node -> next;
+        prev -> next = nxt;
+        nxt -> prev = prev;
     }
 
+    void insert(Node* node){
+        Node* prev = right -> prev;
+        prev -> next = node;
+        node -> prev = prev;
+        node -> next = right;
+        right -> prev = node;
+    }
+
+public:
+    LRUCache(int capacity) {
+        cap = capacity;
+        cache.clear();
+        left = new Node(0, 0);
+        right = new Node(0, 0);
+        left -> next = right;
+        right -> prev = left;
+    }
+    
     int get(int key) {
-        if(m.find(key)==m.end()) return -1;
-        moveToFirst(key);
-        return m[key].second;
+        if(cache.find(key) != cache.end()){
+            Node* node = cache[key];
+            remove(node);
+            insert(node);
+            return node -> val;
+        }
+        return -1;
     }
     
     void put(int key, int value) {
-        if(m.find(key)!=m.end()){
-            m[key].second = value;
-            moveToFirst(key);
+        if (cache.find(key) != cache.end()){
+            remove(cache[key]);
         }
-        else{
-            l.push_front(key);
-            m[key]={l.begin(),value};
-            size--;
-        }
-        if(size<0){
-            m.erase(l.back());
-            l.pop_back();
-            size++;
+        Node* newNode = new Node(key, value);
+        cache[key] = newNode;
+        insert(newNode);
+
+        if(cache.size() > cap){
+            Node* lru = left-> next;
+            remove(lru);
+            cache.erase(lru -> key);
+            delete lru;
         }
     }
 };
